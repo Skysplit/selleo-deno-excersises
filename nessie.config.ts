8404a461b3dd7e08a4e8285f72d856c87bcef579
Skysplit <t.romik@selleo.com>
2022-05-10 21:37:44 +0200

    initialise nessie

---
+++ nessie.config.ts
@@ -0,0 +1,22 @@
+import {
+  ClientPostgreSQL,
+  NessieConfig,
+} from "https://deno.land/x/nessie@2.0.5/mod.ts";
+import "https://deno.land/x/dotenv@v3.2.0/load.ts";
+
+const client = new ClientPostgreSQL({
+  hostname: "localhost",
+  port: 5432,
+  database: Deno.env.get("POSTGRES_DB")!,
+  user: Deno.env.get("POSTGRES_USER")!,
+  password: Deno.env.get("POSTGRES_PASSWORD")!,
+});
+
+/** This is the final config object */
+const config: NessieConfig = {
+  client,
+  migrationFolders: ["./src/db/migrations"],
+  seedFolders: ["./src/db/seeds"],
+};
+
+export default config;
