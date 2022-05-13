4eef5135be631d050770fbf9c142da33c76e0556
Bernadetta Fryczkowska <bfryczkowska@gmail.com>
2022-05-13 12:59:24 +0200

    add validation for post request

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,7 @@
 import "https://deno.land/x/dotenv@v3.2.0/load.ts";
 export { opine, json } from "https://deno.land/x/opine@2.2.0/mod.ts";
 export { Router } from "https://deno.land/x/opine@2.2.0/mod.ts";
+export { validate, required, isString } from "https://deno.land/x/validasaur/mod.ts";
 export {
   Model,
   DataTypes,
--- src/todos.ts
+++ src/todos.ts
@@ -1,5 +1,5 @@
 import { TodoItem } from "/db/models/TodoItem.ts";
-import { Router } from '/deps.ts'
+import { Router, validate, required, isString } from "/deps.ts";
 
 const api = Router();
 
@@ -10,16 +10,30 @@ api.get("/", async function (req, res) {
 });
 
 api.post("/", async function (req, res) {
+  const body = req.parsedBody;
+
+  const [passes, errors] = await validate(body, {
+    title: [required, isString],
+    content: [required, isString],
+  });
+
+  if (passes) {
+    const { title, content } = body;
+    req.body = { title, content };
+  } else {
+    res.json(errors).setStatus(400);
+  }
+
   const response = await TodoItem.create(req.body);
   res.json(response);
 });
 
 api.put("/:id", async function (req, res) {
-  const todoId = req.params.id
-  await TodoItem.where('id', todoId).update(req.body);
-  const response = await TodoItem.find(todoId)
-  res.json(response)
-})
+  const todoId = req.params.id;
+  await TodoItem.where("id", todoId).update(req.body);
+  const response = await TodoItem.find(todoId);
+  res.json(response);
+});
 
 api.delete("/:id", async function (req, res) {
   const todo = await TodoItem.find(req.params.id);
@@ -27,4 +41,4 @@ api.delete("/:id", async function (req, res) {
   res.send(todo);
 });
 
-export default api
+export default api;
