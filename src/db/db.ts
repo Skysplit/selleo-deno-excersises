83e2125953bc7296d2ae224de6f9c9679fa07de1
tomasz-zwak <t.zwak@selleo.com>
2022-05-13 10:38:03 +0200

    link db

--- src/db/db.ts
+++ src/db/db.ts
@@ -1,16 +1,21 @@
 import { PostgresConnector, Database } from "/deps.ts";
 import { TodoItem } from "./models/TodoItem.ts";
 
-const connector = new PostgresConnector({
-  host: "localhost",
-  port: 5432,
-  database: Deno.env.get("POSTGRES_DB")!,
-  username: Deno.env.get("POSTGRES_USER")!,
-  password: Deno.env.get("POSTGRES_PASSWORD")!,
-});
+export const initDb = async () => {
+  const connector = new PostgresConnector({
+    host: "localhost",
+    port: 5432,
+    database: Deno.env.get("POSTGRES_DB")!,
+    username: Deno.env.get("POSTGRES_USER")!,
+    password: Deno.env.get("POSTGRES_PASSWORD")!,
+  });
+  const db = new Database(connector);
 
-export const db = new Database(connector);
+  db.link([TodoItem]);
 
-db.link([TodoItem]);
+  console.log("linked");
 
-await db.sync({ drop: true });
+  await db.sync({ drop: true });
+};
+
+export default initDb;
--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,18 @@
 import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
+import { TodoItem } from "/db/models/TodoItem.ts";
+import { initDb } from "./db/db.ts";
 
+await initDb();
 const app = new Application();
-
 console.log("http://localhost:8080/");
 
-app
-  .get("/hello", (c) => {
-    return "Hello, Abc!";
-  })
-  .start({ port: 8080 });
+app.get("/todoitems", async (req) => {
+  console.log(TodoItem.table);
+  const toDoItems = await TodoItem.all();
+  console.log(toDoItems);
+  const json = JSON.stringify(toDoItems);
+  console.log(json);
+  return json;
+});
+
+app.start({ port: 8080 });
