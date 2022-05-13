7f974ae216540ce5fe39ee68f77df235dd4a5423
tomasz-zwak <t.zwak@selleo.com>
2022-05-13 12:47:58 +0200

    crud

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,6 @@ import { initDb } from "./db/db.ts";
 import { ToDoItemDTO } from "./db/models/todoitem.dto.ts";
 import * as dejs from "https://deno.land/x/dejs@0.9.3/mod.ts";
 
-
 await initDb();
 const app = new Application();
 app.renderer = {
@@ -15,28 +14,34 @@ app.renderer = {
 
 console.log("http://localhost:8080/");
 
-app.get("/todoitems", async (context) => {
- return await TodoItem.all() as unknown as ToDoItemDTO[];
+app.get("/todolist", async (context) => {
+  return (await TodoItem.all()) as unknown as ToDoItemDTO[];
   // const items =  await TodoItem.all() as unknown as ToDoItemDTO[];
   // console.log({items})
   // await context.render("src/items.html", {items: items})
 });
 
-app.get("/todoitems/:id", async (context) => {
+app.get("/todolist/:id", async (context) => {
   const { id } = context.params;
-  const item = await TodoItem.find(id) as unknown as ToDoItemDTO;
-  await context.render("src/index.html", {item: item})
-
+  return (await TodoItem.find(id)) as unknown as ToDoItemDTO;
 });
 
-
-app.post("/", async (req) => {
-  const { title, content, checked } = (await req.body) as ToDoItemDTO;
+app.post("/todolist", async (context) => {
+  const { title, content, checked } = (await context.body) as ToDoItemDTO;
   return await TodoItem.create({ title, content, checked });
 });
 
-app.put("/", async (req) => {});
+app.post("/todolist/:id/toggle", async (context) => {
+  const { id } = context.params;
+  const item = await TodoItem.find(id);
+  item.checked = !item.checked;
+  return await item.update();
+});
 
-app.delete("/", async (req) => {});
+app.delete("/todolist/:id", async (context) => {
+  const { id } = context.params;
+  const item = await TodoItem.find(id);
+  return await item.delete();
+});
 
 app.start({ port: 8080 });
