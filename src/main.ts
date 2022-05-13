606de8c3a58e9c922047c26a6490911728a00c86
Marzena Orawczak <marzena.orawczak@gmail.com>
2022-05-13 11:41:55 +0200

    add renderer

--- src/main.ts
+++ src/main.ts
@@ -2,14 +2,24 @@ import { Application } from "https://deno.land/x/abc@v1.3.3/mod.ts";
 import { TodoItem } from "/db/models/TodoItem.ts";
 import { initDb } from "./db/db.ts";
 import { ToDoItemDTO } from "./db/models/todoitem.dto.ts";
+import * as dejs from "https://deno.land/x/dejs@0.9.3/mod.ts";
+
 
 await initDb();
 const app = new Application();
+app.renderer = {
+  render<T>(name: string, data: T): Promise<Deno.Reader> {
+    return dejs.renderFile(name, data);
+  },
+};
+
 console.log("http://localhost:8080/");
 
-app.get("/todoitems/:id", async (req) => {
-  const { id } = req.params;
-  return await TodoItem.find(id);
+app.get("/todoitems/:id", async (context) => {
+  const { id } = context.params;
+  const item = await TodoItem.find(id);
+  await context.render("index.html", {item: item})
+
 });
 
 app.get("/todoitems", async (req) => {
