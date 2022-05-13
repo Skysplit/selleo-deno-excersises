76916b68a09d51043b5202de599e9cb0ce38b281
Ewa Wydra <e.wydra@selleo.com>
2022-05-13 11:46:46 +0200

    Add test for updating todoItem

--- src/main_test.ts
+++ src/main_test.ts
@@ -1,10 +1,14 @@
 import { app } from './main.ts'
-import { superdeno, describe, it, asserts } from '/dev_deps.ts'
+import { superdeno, describe, it, asserts, beforeEach } from '/dev_deps.ts'
 import { TodoItem } from './db/models/TodoItem.ts'
 
 const { assertEquals, assertObjectMatch } = asserts
 
 describe("/todos", () => {
+  beforeEach(async () => {
+    await TodoItem.truncate()
+  })
+
   it('GET /todos', async () => {
     const todoItem = {
       title: 'Test title',
@@ -19,5 +23,33 @@ describe("/todos", () => {
         const [returnedTodoItem] = res.body
         assertObjectMatch(returnedTodoItem, todoItem)
       });
+  })
+
+  it('PUT /todos:id', async () => {
+    const createdTodoItem = await TodoItem.create({
+      title: 'Test title',
+      content: 'Test content'
+    });
+
+    const updateFields = {
+      title: 'Updated title',
+      checked: true
+    }
+
+    await superdeno(app)
+      .put(`/todos/${createdTodoItem.id}`)
+      .send(updateFields)
+      .expect(res => {
+        const updatedTodoItem = res.body
+        assertEquals(updatedTodoItem.title, updateFields.title)
+        assertEquals(updatedTodoItem.content, createdTodoItem.content)
+        assertEquals(updatedTodoItem.checked, updateFields.checked)
+      })
+
+      const updatedTodoItem = await TodoItem.find(String(createdTodoItem.id))
+      assertEquals(updatedTodoItem.title, updateFields.title)
+      assertEquals(updatedTodoItem.content, createdTodoItem.content)
+      assertEquals(updatedTodoItem.checked, updateFields.checked)
+
   })
 });
